9b405cd863ec40f20e03161798f6639a0a41ebf0
zhengxiangyang <zxy@xinshi.net>
2018-10-24 16:40:39 +0800

    增加wx.request

--- templates/flutter/owl_flutter/lib/utils/wx.dart
+++ templates/flutter/owl_flutter/lib/utils/wx.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart';
 
 import 'owl.dart';
 
@@ -18,4 +19,36 @@ class WeiXinAdapter {
   void switchTab(o) {
     owl.getApplication().switchTab(o, buildContext);
   }
+
+  request(o) {
+    var method = o['method'];
+    if (method == null) {
+      method = 'GET';
+    }
+    var url = o['url'];
+    var data = o['data'];
+    Map<String,String> header = o['header'];
+    Request req = Request(method, url);
+    if (data is String) {
+      req.body = data;
+    } else {
+      req.bodyFields = data;
+    }
+
+    Map<String, String> headers = req.headers;
+    for(MapEntry<String, String> entry in header.entries){
+      headers[entry.key] = entry.value;
+    }
+
+    var client = new Client();
+    client.send(req).then((StreamedResponse response){var res = {};
+      response.stream.bytesToString().then((String body){
+        var res = {};
+        res['data'] = body;
+        res['statusCode'] = response.statusCode;
+        res['header'] = response.headers;
+        o['success'](res);
+      },onError: o['failed']);
+
+    },onError: o['failed']).whenComplete(o['complete']);
 }
